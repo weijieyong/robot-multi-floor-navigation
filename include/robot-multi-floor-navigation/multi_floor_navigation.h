2bfc135ef54d68563c51dbf7dc606bbcd812139a
weijie <yong.weijie6@gmail.com>
2024-02-01 14:52:55 +0800

    feat: added navigate to goal1

--- include/robot-multi-floor-navigation/multi_floor_navigation.h
+++ include/robot-multi-floor-navigation/multi_floor_navigation.h
@@ -4,6 +4,7 @@
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include "actionlib_msgs/GoalStatusArray.h"
 
 
 class MultiFloorNavigation
@@ -11,16 +12,21 @@ class MultiFloorNavigation
 private:
     enum States{
         INIT_POSE,
+        NAV_TO_GOAL,
         DONE
     };
     States state;
     ros::Publisher initial_pose_pub, goal_pub;
-    ros::Subscriber amcl_pose_sub;
+    ros::Subscriber amcl_pose_sub, move_base_status_sub;
     geometry_msgs::PoseWithCovarianceStamped curr_pose;
+    actionlib_msgs::GoalStatusArray move_base_status_msg;
+    bool is_goal_sent, is_goal_active;
 
     void amclPoseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg);
+    void movebaseStatusCallback(const actionlib_msgs::GoalStatusArray::ConstPtr& msg);
     void initializeRobotPose(double x, double y, double z, double yaw);
     tf2::Quaternion yawtoQuartenion(double yaw);
+    void sendSimpleGoal(double x, double y, double z, double yaw);
 
 public:
     MultiFloorNavigation();
--- src/multi_floor_navigation.cpp
+++ src/multi_floor_navigation.cpp
@@ -2,7 +2,9 @@
 
 
 MultiFloorNavigation::MultiFloorNavigation() :
-    state(MultiFloorNavigation::States::INIT_POSE)
+    state(MultiFloorNavigation::States::INIT_POSE),
+    is_goal_sent(false),
+    is_goal_active(false)
 {
 }
 
@@ -17,10 +19,16 @@ void MultiFloorNavigation::initialize(ros::NodeHandle& n){
     goal_pub = n.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 1);
     
     amcl_pose_sub = n.subscribe("amcl_pose", 1, &MultiFloorNavigation::amclPoseCallback, this);
+    move_base_status_sub = n.subscribe("move_base/status", 1, &MultiFloorNavigation::movebaseStatusCallback, this);
+
 }
 
 void MultiFloorNavigation::amclPoseCallback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
     curr_pose.pose = msg->pose;
+}
+
+void MultiFloorNavigation::movebaseStatusCallback(const actionlib_msgs::GoalStatusArray::ConstPtr& msg){
+    move_base_status_msg = *msg;
 }
 
 tf2::Quaternion MultiFloorNavigation::yawtoQuartenion(double yaw)
@@ -52,11 +60,55 @@ void MultiFloorNavigation::initializeRobotPose(double x, double y, double z, dou
     initial_pose_pub.publish(initial_pose);
 }
 
-void MultiFloorNavigation::execute(){
+void MultiFloorNavigation::sendSimpleGoal(double x, double y, double z, double yaw)
+{
+    geometry_msgs::PoseStamped goal;
+
+    goal.header.frame_id = "map";
+    goal.header.stamp = ros::Time::now();
+
+    goal.pose.position.x = x;
+    goal.pose.position.y = y;
+    goal.pose.position.z = z;
+
+    tf2::Quaternion q = yawtoQuartenion(yaw);
+
+    goal.pose.orientation.x = q[0];
+    goal.pose.orientation.y = q[1];
+    goal.pose.orientation.z = q[2];
+    goal.pose.orientation.w = q[3];
+
+    ROS_INFO("Sending robot to x: %.1f, y: %.1f, z: %.1f, yaw: %.1f", x, y, z, yaw);
+    goal_pub.publish(goal);
+}
+
+void MultiFloorNavigation::execute()
+{
     switch(state){
         case MultiFloorNavigation::States::INIT_POSE:
+            ROS_INFO_ONCE("Initializing robot at x:4.0, y:-5.0, z:0.5, yaw:0.0deg");
             initializeRobotPose(4.0, -5.0, 0.5, 0.0); // TODO: hard-coded
-            state = MultiFloorNavigation::States::DONE;
+            state = MultiFloorNavigation::States::NAV_TO_GOAL;
+            break;
+
+        case MultiFloorNavigation::States::NAV_TO_GOAL:
+            ROS_INFO_ONCE("Sending goal1");
+            if(!is_goal_sent) {
+                sendSimpleGoal(3.0, -0.5, 0.5, M_PI);
+                is_goal_sent = true;
+            }
+            if(!move_base_status_msg.status_list.empty()) {
+                auto& currentStatus = move_base_status_msg.status_list[0].status;
+
+                if(currentStatus == actionlib_msgs::GoalStatus::ACTIVE) {
+                    is_goal_active = true;
+                }
+                else if(is_goal_active && 
+                    currentStatus == actionlib_msgs::GoalStatus::SUCCEEDED) {
+                    state = MultiFloorNavigation::States::DONE;
+                    is_goal_sent = is_goal_active = false;
+                }
+            }
             break;
 
         case MultiFloorNavigation::States::DONE:
