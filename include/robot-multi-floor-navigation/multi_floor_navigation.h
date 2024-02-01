bcbfded16513bfc19bad8d70a5cee6574772c0c7
weijie <yong.weijie6@gmail.com>
2024-02-01 22:32:07 +0800

    feat: add elevator related state

--- include/robot-multi-floor-navigation/multi_floor_navigation.h
+++ include/robot-multi-floor-navigation/multi_floor_navigation.h
@@ -5,6 +5,8 @@
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include "actionlib_msgs/GoalStatusArray.h"
+#include "std_msgs/String.h"
+#include <geometry_msgs/Twist.h>
 
 
 class MultiFloorNavigation
@@ -13,10 +15,14 @@ private:
     enum States{
         INIT_POSE,
         NAV_TO_GOAL,
+        CALL_ELEVATOR_0,
+        ENTER_ELEVATOR_0,
+        CALL_ELEVATOR_1,
+        EXIT_ELEVATOR_1,
         DONE
     };
     States state;
-    ros::Publisher initial_pose_pub, goal_pub;
+    ros::Publisher initial_pose_pub, goal_pub, elevator_pub, cmd_vel_pub;
     ros::Subscriber amcl_pose_sub, move_base_status_sub;
     geometry_msgs::PoseWithCovarianceStamped curr_pose;
     actionlib_msgs::GoalStatusArray move_base_status_msg;
@@ -27,6 +33,8 @@ private:
     void initializeRobotPose(double x, double y, double z, double yaw);
     tf2::Quaternion yawtoQuartenion(double yaw);
     void sendSimpleGoal(double x, double y, double z, double yaw);
+    void callElevatorFloor(std::string floor);
+    void publishCmdVel(double linear_vel, double angular_vel);
 
 public:
     MultiFloorNavigation();
--- src/multi_floor_navigation.cpp
+++ src/multi_floor_navigation.cpp
@@ -17,6 +17,8 @@ void MultiFloorNavigation::initialize(ros::NodeHandle& n){
 
     initial_pose_pub = n.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 1, true);
     goal_pub = n.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 1);
+    elevator_pub = n.advertise<std_msgs::String>("elevator", 1, true);
+    cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10, true);
     
     amcl_pose_sub = n.subscribe("amcl_pose", 1, &MultiFloorNavigation::amclPoseCallback, this);
     move_base_status_sub = n.subscribe("move_base/status", 1, &MultiFloorNavigation::movebaseStatusCallback, this);
@@ -51,10 +53,10 @@ void MultiFloorNavigation::initializeRobotPose(double x, double y, double z, dou
 
     tf2::Quaternion q = yawtoQuartenion(yaw);
 
-    initial_pose.pose.pose.orientation.x= q[0];
-    initial_pose.pose.pose.orientation.y= q[1];
-    initial_pose.pose.pose.orientation.z= q[2];
-    initial_pose.pose.pose.orientation.w= q[3];
+    initial_pose.pose.pose.orientation.x = q[0];
+    initial_pose.pose.pose.orientation.y = q[1];
+    initial_pose.pose.pose.orientation.z = q[2];
+    initial_pose.pose.pose.orientation.w = q[3];
 
     ROS_INFO("Initializing robot at x: %.1f, y: %.1f, z: %.1f, yaw: %.1f", x, y, z, yaw);
     initial_pose_pub.publish(initial_pose);
@@ -82,16 +84,37 @@ void MultiFloorNavigation::sendSimpleGoal(double x, double y, double z, double y
     goal_pub.publish(goal);
 }
 
+void MultiFloorNavigation::callElevatorFloor(std::string floor)
+{
+    std_msgs::String floor_no;
+    floor_no.data = floor;
+    elevator_pub.publish(floor_no);
+}
+
+void MultiFloorNavigation::publishCmdVel(double linear_vel=0.0, double angular_vel=0.0){
+    geometry_msgs::Twist cmd_vel;
+
+    cmd_vel.linear.x = linear_vel;
+    cmd_vel.linear.y = 0;
+    cmd_vel.linear.z = 0;
+
+    cmd_vel.angular.x = 0;
+    cmd_vel.angular.y = 0;
+    cmd_vel.angular.z = angular_vel;
+
+    cmd_vel_pub.publish(cmd_vel);
+}
+
 void MultiFloorNavigation::execute()
 {
     switch(state){
-        case MultiFloorNavigation::States::INIT_POSE:
+        case States::INIT_POSE:
             ROS_INFO_ONCE("Initializing robot at x:4.0, y:-5.0, z:0.5, yaw:0.0deg");
             initializeRobotPose(4.0, -5.0, 0.5, 0.0); // TODO: hard-coded
-            state = MultiFloorNavigation::States::NAV_TO_GOAL;
+            state = States::NAV_TO_GOAL;
             break;
 
-        case MultiFloorNavigation::States::NAV_TO_GOAL:
+        case States::NAV_TO_GOAL:
             ROS_INFO_ONCE("Sending goal1");
             if(!is_goal_sent) {
                 sendSimpleGoal(3.0, -0.5, 0.5, M_PI);
@@ -105,13 +128,51 @@ void MultiFloorNavigation::execute()
                 }
                 else if(is_goal_active && 
                     currentStatus == actionlib_msgs::GoalStatus::SUCCEEDED) {
-                    state = MultiFloorNavigation::States::DONE;
+                    state = States::CALL_ELEVATOR_0;
                     is_goal_sent = is_goal_active = false;
                 }
             }
             break;
 
-        case MultiFloorNavigation::States::DONE:
+        case States::CALL_ELEVATOR_0:
+            ROS_INFO_ONCE("Calling elevator at Floor 0");
+            callElevatorFloor("0");
+            state = States::ENTER_ELEVATOR_0;
+            break;
+
+        case States::ENTER_ELEVATOR_0:
+            ROS_INFO_ONCE("Entering elevator at Floor 0");
+            
+            if(fabs(curr_pose.pose.pose.position.x - 0.5) < 0.1) { // TODO: hard coded tolerance
+                publishCmdVel(0.0, 0.0);
+                ros::Duration(7).sleep(); // wait for elevator to close
+                state = States::CALL_ELEVATOR_1;
+            }
+            else {
+                publishCmdVel(0.3, 0.0);
+            }
+            break;
+
+        case States::CALL_ELEVATOR_1:
+            ROS_INFO_ONCE("Calling elevator to Floor 1");
+            callElevatorFloor("1");
+            ros::Duration(7).sleep(); // wait for elevator to travel to level 1 and open
+            state = States::EXIT_ELEVATOR_1;
+            break;
+
+        case States::EXIT_ELEVATOR_1:
+            ROS_INFO_ONCE("Exiting elevator at Floor 1");
+            
+            if(fabs(curr_pose.pose.pose.position.x - 3.0) < 0.1) { // TODO: hard coded tolerance
+                publishCmdVel(0.0, 0.0);
+                state = States::DONE;
+            }
+            else {
+                publishCmdVel(-0.3, 0.0);
+            }
+            break;
+
+        case States::DONE:
             ROS_INFO_ONCE("Robot reached goal");
             break;
 
